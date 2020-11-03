68786b19662ce7167078759369fb1ebb42b6ba24
이동혁 <donghyeok@DongMacBookPro2019.local>
2020-11-03 16:12:03 +0900

    _

--- models/beta_vae.py
+++ models/beta_vae.py
@@ -81,7 +81,7 @@ class BetaVAE(BaseVAE):
                                                output_padding=1),
                             nn.BatchNorm2d(hidden_dims[-1]),
                             nn.LeakyReLU(),
-                            nn.Conv2d(hidden_dims[-1], out_channels= 3,
+                            nn.Conv2d(hidden_dims[-1], out_channels= 1,
                                       kernel_size= 3, padding= 1),
                             nn.Tanh())
 
